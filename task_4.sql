3e05b72f16641750bf07e44a950404b8fbb56239
Maruf Balogun <marufbyte@gmail.com>
2025-11-23 16:24:12 +0100

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,2 +1,11 @@
--- Display the full structure of the 'Books' table
-SHOW CREATE TABLE Books;
+USE alx_book_store;
+
+SELECT COLUMN_NAME,
+       COLUMN_TYPE,
+       IS_NULLABLE,
+       COLUMN_KEY,
+       COLUMN_DEFAULT,
+       EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'books';
